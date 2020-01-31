33c97f86c1a7e054b41668f7a6cbf95e9c6442d5
Jakub Turek <jkbturek@gmail.com>
2020-01-31 12:05:50 +0100

    Present how to use Danger DSL

--- example_danger_plugin/plugin.py
+++ example_danger_plugin/plugin.py
@@ -3,4 +3,9 @@ from danger_python.plugins import DangerPlugin
 
 class ExampleDangerPlugin(DangerPlugin):
     def hello(self):
-        self.message("Hello world!")
+        created = len(self.danger.git.created_files)
+        modified = len(self.danger.git.modified_files)
+        deleted = len(self.danger.git.deleted_files)
+        self.message(f"Created files count: {created}")
+        self.message(f"Modified files count: {modified}")
+        self.message(f"Deleted files count: {deleted}")
